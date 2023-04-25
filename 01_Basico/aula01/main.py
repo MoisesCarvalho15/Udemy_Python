35ebc15da0648c4c6a1129cc7b52184d452d43ee
Moises Santos Carvalho <mscarvalho1501@gmail.com>
2023-04-24 22:03:31 -0300

    Programa 01 - Comando print e comentários no programa

---
+++ 01_Basico/aula01/main.py
@@ -0,0 +1,11 @@
+# Comentários não são executados
+
+'''
+Podemos fazer comentários em uma única linha utilizando o '#' 
+ou em múltiplas linhas utilizando 
+três aspas simples ou duplas em sequência.
+'''
+
+print('O comando print é uma função que mostra os dados na tela.')
+print('Os textos podem ser escritos em aspas simples')
+print("Ou em aspas duplas.")
