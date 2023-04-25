60b3ef95cb829e8db2e5e7ad3a744005aad48930
Moises Santos Carvalho <mscarvalho1501@gmail.com>
2023-04-24 22:27:04 -0300

    Programa 02 - Tipos de dados

---
+++ 01_Basico/aula02/main.py
@@ -0,0 +1,13 @@
+# Tipos de Dados
+
+a = False       # Boolean
+b = True        # Boolean
+c = str(3)      # texto
+d = int(4)      # inteiro
+e = float(5)    # fração
+
+print(a)
+print(b)
+print(c + c)  # concatenação de strings(textos)
+print(d + d)  # soma dos números inteiros
+print(e + e)  # casa decimais.
