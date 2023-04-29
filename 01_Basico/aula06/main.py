18ce457d01a18d3a2ea4a16ec9022f9289403233
Moisés Santos Carvalho <mscarvalho1501@gmail.com>
2023-04-29 19:45:39 -0300

    Utilizando  o formatted strings

---
+++ 01_Basico/aula06/main.py
@@ -0,0 +1,11 @@
+# Formatted Strings
+
+nome = input('Digite o seu nome: ')
+sobrenome = input('Digite o seu sobrenome: ')
+profissao = input('Digite a sua profissão: ')
+
+# Utilizando o formatted strings para juntar variáveis em uma frase
+frase = f'{nome} {sobrenome} tem como profissão ser {profissao}.'
+
+print(frase)
+ 
