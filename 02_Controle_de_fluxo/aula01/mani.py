1b43bb19d461182ab4995ca315cb2e7eb3149aa3
Moisés Santos Carvalho <mscarvalho1501@gmail.com>
2023-05-02 22:30:38 -0300

    If, elif and else na prática

---
+++ 02_Controle_de_fluxo/aula01/mani.py
@@ -0,0 +1,13 @@
+# if, elif and else
+
+velocidade = 110
+
+if velocidade >= 110:
+    print('Velocidade acima do permitido.')
+    print('Favor, reduzir sua velocidade.')
+elif velocidade < 60:
+    print('Velocidade abaixo do permitido')
+    print('Favor, dirigir acima de 80km/h')
+else:
+    print('Velocidade Permitida. Continue assim.')
+    
