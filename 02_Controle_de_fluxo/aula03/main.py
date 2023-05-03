b22e054e74c46454c4121df9ec01a14ca8137d99
Moisés Santos Carvalho <mscarvalho1501@gmail.com>
2023-05-02 22:38:01 -0300

    Operador Ternário

---
+++ 02_Controle_de_fluxo/aula03/main.py
@@ -0,0 +1,12 @@
+# Operador Ternário
+
+idade = 15
+
+# if idade >= 16:
+#     print('Voto Permitido!')
+# else:
+#     print('Voto NÃO Permitido!')
+    
+# Utilizando o operador ternário
+resultado = 'Voto Permitido!' if idade >= 16 else 'Voto NÃO Permitido!'
+print(resultado)
