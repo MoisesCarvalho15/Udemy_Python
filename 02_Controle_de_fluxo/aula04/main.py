6719544b05ab0f10c2dda5f3a950260b4d809611
Moisés Santos Carvalho <mscarvalho1501@gmail.com>
2023-05-03 22:27:08 -0300

    Múltiplos Operadores de comparação

---
+++ 02_Controle_de_fluxo/aula04/main.py
@@ -0,0 +1,15 @@
+# Múltiplos Operadores de comparação
+
+numero = 0
+numero2 = 25
+
+if numero >= 0 and numero < 20:
+    print('O número está entre 0 e 19!')
+else:
+    print('O número NÃO está entre 0 e 19!')
+    
+# Outra forma
+if 5 <= numero2 < 26:
+    print('O número está entre 5 e 25!')
+else:
+    print('O número NÃO está entre 5 e 25!')
