99cde6f1d0ef1a677342fcc1cc02ea66bd9ba145
Moisés Santos Carvalho <mscarvalho1501@gmail.com>
2023-05-06 20:42:36 -0300

    For loop - Separando letras de uma String

---
+++ 02_Controle_de_fluxo/aula09/main.py
@@ -0,0 +1,11 @@
+"""
+== For Loop - Separando letras de uma Strings ==
+
+* Colocar espaçamento em cada letra de uma palavra
+* Exemplo: PYTHON == P Y T H O N
+"""
+
+palavra = 'Python'
+
+for letra in palavra:
+    print(f'{letra} ', end= '')
