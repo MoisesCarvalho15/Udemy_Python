5b5db7a4821f4df4b12c808ac575f0ac8b71d563
Moisés Santos Carvalho <mscarvalho1501@gmail.com>
2023-05-07 13:35:01 -0300

    Função com Argumentos e Parâmetros

---
+++ 03_Funcoes/aula02/main.py
@@ -0,0 +1,9 @@
+# Funções com Argumentos e Parâmetros
+
+def boas_vindas(nome, idade):
+    print(f'Seu nome é {nome} e você tem {idade} anos!')    
+
+
+boas_vindas('Moisés', 22)
+boas_vindas('Clara', 19)
+boas_vindas('Carlos', 27)
