59816eb0d5fb8cc94e5fe8912b930d55594aca58
Moisés Santos Carvalho <mscarvalho1501@gmail.com>
2023-05-07 13:34:24 -0300

    Criando Função sem Parâmetros

---
+++ 03_Funcoes/aula01/main.py
@@ -0,0 +1,17 @@
+# Função Sem parâmetros
+
+def somar():
+    n1 = 10
+    n2 = 20
+    resultado = n1 + n2
+    print(f'{n1} + {n2} = {resultado}')
+
+def subtrair():
+    n1 = 20
+    n2 = 2
+    resultado = n1 - n2
+    print(f'{n1} - {n2} = {resultado}')
+
+
+somar()
+subtrair()
