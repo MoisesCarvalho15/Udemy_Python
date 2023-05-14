3900e03da25bff05a8d3123604047190f959d084
Moisés Santos Carvalho <mscarvalho1501@gmail.com>
2023-05-14 20:16:42 -0300

    Manipulando listas

---
+++ 04_Estrutura_de_dados/aula01/main.py
@@ -0,0 +1,15 @@
+# Manipulando listas
+
+# Armazenando vários dados em uma variável
+# index       0          1         2        3        4
+frutas = ['Abacate', 'Abacaxi', 'Amora', 'Banana', 'Maça']
+
+# Imprimindo toda a lista
+print(frutas)
+
+# Pegando um item da listas
+print(f'Primeiro item da lista: {frutas[0]}')
+
+# Trocando o nome de um item da lista
+frutas[2] = 'Caqui'
+print(frutas)
