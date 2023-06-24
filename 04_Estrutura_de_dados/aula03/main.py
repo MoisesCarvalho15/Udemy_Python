ccc11cb307fd82e661e325bd4145bdb3c8d33e2d
Moises Santos Carvalho <mscarvalho1501@gmail.com>
2023-06-24 20:43:44 -0300

    Aula 03 - Listas dentro de Lista

---
+++ 04_Estrutura_de_dados/aula03/main.py
@@ -0,0 +1,9 @@
+# Lista dentro de Lista
+itens = [['Item 1', 'Item 2'], ['Item 3', 'Item 4']]
+
+primeira_lista = itens[0]
+segunda_lista = itens[1]
+
+print(f'Quantidade de listas dentro da lista itens: {len(itens)}')
+print(f'Itens da primeira lista: {primeira_lista}')
+print(f'Itens da segunda lista: {segunda_lista}')
