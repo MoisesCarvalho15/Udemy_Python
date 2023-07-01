6abd3eaabe195f94e7bf17cfb2dfa8313a62d09d
Moises Santos Carvalho <mscarvalho1501@gmail.com>
2023-06-30 21:48:59 -0300

    Criando e imprimindo um dicionário

---
+++ 04_Estrutura_de_dados/aula11/main.py
@@ -0,0 +1,14 @@
+"""
+# Trabalhando com dicionários
+    - Para criar dicionários usamos chaves: {}
+    - Utiliza o index no formato Keys and Values
+    - Aceita String, integer, float, boolean...
+"""
+
+# Criando um dicionário
+aluno = {'nome': 'Moisés', 'idade': 22, 'CNH': False}
+
+print(aluno)
+
+# Imprimindo o valor através da Key
+print(aluno['nome'])
