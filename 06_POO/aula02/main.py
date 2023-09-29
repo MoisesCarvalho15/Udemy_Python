dac243d7650831e2beadd02224272449687ec114
Moises Santos Carvalho <mscarvalho1501@gmail.com>
2023-09-29 06:47:36 -0300

    Aula 02 - Criando parâmetros separados do objeto

---
+++ 06_POO/aula02/main.py
@@ -0,0 +1,23 @@
+# Criando parâmetros
+
+# Classe
+class Funcionarios:
+    pass
+
+# Objetos
+user1 = Funcionarios()
+user2 = Funcionarios()
+
+# Parâmetros do usuário 1
+user1.nome = 'Marcos'
+user1.sobrenome = 'Pereira'
+user1.data_nascimento = '10/05/2005'
+
+# Parâmetros do usuário 2
+user2.nome = 'Maria'
+user2.sobrenome = 'Santos'
+user2.data_nascimento = '07/07/1988'
+
+# Mostrando os dados
+print(user1.nome)
+print(user2.nome)
