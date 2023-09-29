6895ca26cdd49bd2d92914f07bf56fcfff42b1b7
Moises Santos Carvalho <mscarvalho1501@gmail.com>
2023-09-29 06:36:04 -0300

    Aula 01 - Criando minha primeira Classe

---
+++ 06_POO/aula01/main.py
@@ -0,0 +1,12 @@
+# Criando minha primeira Classe
+class Funcionarios:
+    nome = 'Elena'
+    sobrenome = 'Silva'
+    data_nascimento = '15/08/2000'
+
+
+# Criando objeto e acessando os dados
+user1 = Funcionarios()
+print(user1.nome)
+print(user1.sobrenome)
+print(user1.data_nascimento)
