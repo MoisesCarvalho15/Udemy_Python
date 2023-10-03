20cd3296d214025bde68f7e4093b86602d513542
Moises Santos Carvalho <mscarvalho1501@gmail.com>
2023-10-03 09:23:20 -0300

    Ex03 - Dividindo dois números

---
+++ 08_Exercicios/ex01_a_10/ex03.py
@@ -0,0 +1,12 @@
+"""
+Declare três variáveis: num1, num2, resultado
+num1 -> Deve ser um número inteiro
+num2 -> Deve ser um número de ponto flutuante
+Faça uma divisão do num1 por num2
+"""
+
+num1 = 25
+num2 = 4.5
+resultado = num1 / num2
+
+print(resultado)
