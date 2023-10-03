1cab8dd89b87508a019b414577c8ec353809df9e
Moises Santos Carvalho <mscarvalho1501@gmail.com>
2023-10-03 09:30:07 -0300

    Ex04 - Solicitando nome e idade ao usuário

---
+++ 08_Exercicios/ex01_a_10/ex04.py
@@ -0,0 +1,10 @@
+"""
+Crie um programa que pergunte o nome do usuário e sua idade.
+O programa deve imprimir a mensagem:
+'O cliente "nome" tem "idade" anos.'
+"""
+
+nome = input('Digite seu nome: ')
+idade = int(input('Digite sua idade: '))
+
+print(f'O cliente {nome} tem {idade} anos.')
