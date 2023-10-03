3ef1e5dcdb4ddffe6568d70968f0882d1d8f7f86
Moises Santos Carvalho <mscarvalho1501@gmail.com>
2023-10-03 08:33:03 -0300

    Ex02 - Declarando variáveis

---
+++ 08_Exercicios/ex01_a_10/ex02.py
@@ -0,0 +1,10 @@
+"""
+Declare duas variáveis: nome, idade
+Crie um programa que imprima a frase abaixo substituindo o x pelas variáveis correspondentes: 
+'Olá, meu nome é X e eu tenho X anos.
+"""
+
+nome = 'Moisés'
+idade = 22
+
+print(f'Olá, meu nome é {nome} e eu tenho {idade} anos!')
