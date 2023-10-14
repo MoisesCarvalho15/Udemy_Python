f3013fedafcb7c56affbbae34d84689cbd5b305c
Moises Santos Carvalho <mscarvalho1501@gmail.com>
2023-10-14 08:30:21 -0300

    Ex12 - Criando combinações utilizando Nasted for loop

---
+++ 08_Exercicios/ex11_a_20/ex12.py
@@ -0,0 +1,11 @@
+"""
+Crie um programa que imprima todas as combinações possíveis entre duas listas.
+"""
+
+frutas = ['Maçã', 'Morango', 'Uva']
+vegetais = ['Chuchu', 'Alface', 'Couve']
+
+for fruta in frutas:
+    for vegetal in vegetais:
+        print(fruta, vegetal)
+    print()
