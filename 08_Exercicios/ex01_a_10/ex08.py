a102ed7396b3bee35c79d6a062d25c9d9e41cc06
Moises Santos Carvalho <mscarvalho1501@gmail.com>
2023-10-14 07:29:08 -0300

    Ex08 - Modificando e adicionando itens na lista

---
+++ 08_Exercicios/ex01_a_10/ex08.py
@@ -0,0 +1,15 @@
+"""
+Crie um programa para alterar os itens da lista de frutas.
+Altere o segundo item da lista para "Morango".
+Adicione a fruta "Abacaxi" ao final da lista.
+Imprima a lista original e a modificada.
+"""
+
+frutas = ['Maçã', 'Banana', 'Manga', 'Uva']
+
+print(frutas)
+
+frutas[1] = "Morango"
+frutas.append('Abacaxi')
+
+print(frutas)
