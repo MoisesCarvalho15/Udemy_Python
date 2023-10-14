bfdf8855fb80b422d1952109af77e394b96e24a4
Moises Santos Carvalho <mscarvalho1501@gmail.com>
2023-10-14 07:48:45 -0300

    Ex10 - Utilizando loop em lista

---
+++ 08_Exercicios/ex01_a_10/ex10.py
@@ -0,0 +1,9 @@
+"""
+Crie um programa que percorra todos os itens da lista.
+Imprima esses itens com o texto: Eu gosto de "nome da fruta"
+"""
+
+frutas = ['Maçã', 'Morango', 'Uva']
+
+for fruta in frutas:
+    print(f'Eu gosto de {fruta}')
