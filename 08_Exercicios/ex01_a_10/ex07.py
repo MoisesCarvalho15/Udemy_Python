1a342292118c2a1204be44e9646b9cbe80964974
Moises Santos Carvalho <mscarvalho1501@gmail.com>
2023-10-14 07:27:56 -0300

    Ex07 - Acessando os itens da lista

---
+++ 08_Exercicios/ex01_a_10/ex07.py
@@ -0,0 +1,9 @@
+"""
+Crie um programa que imprima a primeira e a última fruta da lista.
+Utilize a lista criada no exercício anterior (ex06).
+"""
+
+frutas = ['Maçã', 'Banana', 'Manga', 'Uva']
+
+print(f'A primeira fruta é: {frutas[0]}')
+print(f'A última fruta é: {frutas[-1]}')
