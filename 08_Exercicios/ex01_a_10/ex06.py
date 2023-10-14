8bd89cb3587c8382f897d38dbfb59ae54433f5d1
Moises Santos Carvalho <mscarvalho1501@gmail.com>
2023-10-14 07:27:01 -0300

    Ex06 - Criando e imprimindo uma lista

---
+++ 08_Exercicios/ex01_a_10/ex06.py
@@ -0,0 +1,9 @@
+"""
+Crie um programa que contenha uma lista de frutas.
+Os itens da lista são: Maçã, banana, manga e uva.
+Imprima essa lista na tela.
+"""
+
+frutas = ['Maçã', 'Banana', 'Manga', 'Uva']
+
+print(frutas)
